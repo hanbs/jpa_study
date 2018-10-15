781fb559304e846c6de572d9bba644430617d659
Jong <freesetting@naver.com>
2018-10-15 16:22:24 +0900

    #Setting JPA

---
+++ src/main/java/com/jpa/study/jpastudy/JpastudyApplication.java
@@ -0,0 +1,12 @@
+package com.jpa.study.jpastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpastudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpastudyApplication.class, args);
+    }
+}
